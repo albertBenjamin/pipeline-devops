456bf53a361c1167d83fb67e59a4ee28daa3df18
Albert Munoz <albert.munozp@gmail.com>
2020-12-18 22:16:43 -0300

    fix call method

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -20,7 +20,7 @@ def call(){
 						break 
 					}
 
-					ejecucion+.callBuildandTest()
+					gradle.callBuildandTest()
 					
 					stage('sonar') {
                         def scannerHome = tool 'sonar-scanner';
@@ -29,7 +29,7 @@ def call(){
                          }                     
                     }
 					
-					ejecucion+.callRun()
+					gradle.callRun()
 
 					stage('rest'){
 						 sleep 10
