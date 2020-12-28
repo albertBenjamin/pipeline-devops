65189832a578f3e7d9e1f31cf291510e1c6cf235
Albert Munoz <albert.munozp@gmail.com>
2020-12-28 18:32:04 -0300

    error nexus

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -3,7 +3,7 @@ def callNexus(filepathString){
 	stage('nexus'){
 							nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', 
 							packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', 
-							filePath: '${filepathString}']], 
+							filePath: filepathString]], 
 							mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
 
 						}
