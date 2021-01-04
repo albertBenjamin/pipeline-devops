6506243b8dba855c438d76a4ce1f4c4aa24fc850
Albert Munoz <albert.munozp@gmail.com>
2021-01-04 18:57:55 -0300

    fix maven

--- vars/continuousIntegration.groovy
+++ vars/continuousIntegration.groovy
@@ -48,7 +48,7 @@ switch(run) {
     // Ejecuta stages ingresadas.
     for (String values : stagesList) {
       println 'Ejecutando etapa ' + values;
-      maven value.trim();
+      maven values.trim();
     }
 // Validación de input de stages vacío o con espacios en blanco, para ejecutar todas las stages.
   } else if (!stagesList || stagesListEmpty) {
