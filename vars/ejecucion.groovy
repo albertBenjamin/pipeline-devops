9a4ace24485154e2099444889cd29d59bf03a978
Albert Munoz <albert.munozp@gmail.com>
2021-01-04 13:09:45 -0300

    sin slack

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -56,14 +56,14 @@ def call(){
         }
 
     }
-    post {
+    /*post {
         success{
 			slackSend channel: 'U01DD0BR7H8', color: 'good', message: 'Ejecución exitosa :'+['Albert Muñoz ']+[env.JOB_NAME]+[params.buildtool], teamDomain: 'dipdevopsusach2020', tokenCredentialId: 'slack'
         }
         failure{
             slackSend channel: 'U01DD0BR7H8', color: 'danger', message: 'Ejecución fallida :'+['Albert Muñoz ']+[env.JOB_NAME]+[params.buildtool]+' en stage' + [env.STAGE_NAME], teamDomain: 'dipdevopsusach2020', tokenCredentialId: 'slack'
         }
-    }
+    }*/
 }
 
 
