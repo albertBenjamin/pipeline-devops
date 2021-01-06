34673ae8623708279e9fa00e8f29d7c8071fa833
Albert Munoz <albert.munozp@gmail.com>
2021-01-06 17:01:04 -0300

    fix path

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -45,7 +45,7 @@ def rest(){
 def nexusCICD(){
 	nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', 
 							packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', 
-							filePath: pathJar]], 
+							filePath: pathJar+'DevOpsUsach2020-0.0.1.jar']], 
 							mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
 }
 
